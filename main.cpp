7671464e3d5d3458aaad586ea18224c4b803e53a
davidegaribaldi <100833454+davidegaribaldi@users.noreply.github.com>
2022-04-01 18:55:43 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-   return 0;
+int opzione=0;
+float a=0,b=0;
+cin>>a>>b;
+cin>>opzione;
+switch(opzione)
+{
+    case 0:
+      cout<<(a*b)/2;
+      break;
+    case 1:
+      cout<<a*a;
+      break;
+    case 2:
+      cout<<a*b;
+      break;
+    default:
+      cout<<"opzione non valida";
+}
+    return 0;
 }
